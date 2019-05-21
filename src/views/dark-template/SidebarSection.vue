4a21188978f74d84c2142fc667336dece6f51f9b
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-05-21 20:12:12 +0430

    Add sidebar section and items component

---
+++ src/views/dark-template/SidebarSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title">
+      {{ title }}
+    </div>
+    <slot v-if="$slots.default" />
+    <sidebar-section-item
+      v-for="(item, index) in items"
+      :key="index"
+      :item="item"
+    />
+  </div>
+</template>
+
+<script>
+import SidebarSectionItem from '@/views/dark-template/SidebarSectionItem'
+export default {
+  name      : 'SidebarSection',
+  components: { SidebarSectionItem },
+  props     : {
+    title: { type: String, default: 'Section' },
+    items: { type: Array, default: () => {} },
+  },
+}
+</script>
+
+<style scoped>
+.title {
+    border-bottom: 2px #bfbfbf solid;
+}
+</style>
---
+++ src/views/dark-template/SidebarSectionItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout>
+    <v-flex
+      v-if="hasIcon"
+      xs1
+    >
+      <v-icon>
+        {{ item.icon }}
+      </v-icon>
+    </v-flex>
+    <v-flex
+      :xs11="hasIcon"
+      :xs12="!hasIcon"
+    >
+      <strong>
+        {{ item.name }}
+      </strong>
+      <p>
+        {{ item.text }}
+      </p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name    : 'SidebarSectionItem',
+  props   : { item: { type: Object, default: () => {} } },
+  computed: {
+    hasIcon () {
+      return !!this.item.icon
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
