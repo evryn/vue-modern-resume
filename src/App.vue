e16785b674844e1f6d9e8cc5579534c8a3d4b680
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-05-21 19:13:42 +0430

    Add container for dark-template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
     <v-content>
-      <hello-world />
+      <dark-template-container />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import DarkTemplateContainer from '@/views/dark-template/Container'
 
 export default {
   name      : 'App',
-  components: { HelloWorld },
+  components: { DarkTemplateContainer },
   data () {
     return {}
   },
---
+++ src/views/dark-template/Container.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app id="dark-template">
+    <v-fade-transition
+      mode="out-in"
+      type="animation"
+    >
+      <v-content>
+        <v-container
+          fluid
+          fill-height
+        >
+          <v-layout
+            align-center
+            justify-center
+          >
+            <v-flex
+              xs12
+              sm8
+              md4
+            >
+              asdasd fas f
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-fade-transition>
+  </v-app>
+</template>
+
+<script>
+export default { name: 'DarkTemplateContainer' }
+</script>
+
+<style scoped>
+#dark-template {
+height: 100%;
+background: url("/img/backgrounds/city.jpg") no-repeat center;
+background-size: cover;
+}
+</style>
