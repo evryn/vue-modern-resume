aee8c6463688a5064031b8f2bac7662c4b3b4c96
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-05-21 18:43:02 +0430

    Change linting engine

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
+  env : {
+    node   : true,
+    browser: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'plugin:vue/recommended',
+    'adenvt',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+  parserOptions: { parser: 'babel-eslint' },
+  globals      : { vm: true },
 }
