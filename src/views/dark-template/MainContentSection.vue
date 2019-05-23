49b6aeed1fdd9dff9cf9328ba6aa82d789e71e4c
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-05-23 05:07:37 +0430

    Add main content section container

---
+++ src/views/dark-template/MainContentSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mb-5">
+    <div class="title mb-3">
+      {{ title }}
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name      : 'MainContentSection',
+  components: { },
+  props     : { title: { type: String, default: 'Section' } },
+}
+</script>
+
+<style scoped>
+.title {
+  border-bottom: 2px #bfbfbf solid;
+  line-height: 1.5 !important;
+}
+</style>
