781cfddbf48d7d70fc3fc951a19c4f92167a4462
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-05-26 23:40:17 +0430

    Publishing build

--- src/plugins/vuetify.js
+++
@@ -1,8 +0,0 @@
-// If you want to load @mdi/font locally instead of using CDN in index.html, uncomment below:
-// import '@mdi/font/css/materialdesignicons.css'
-
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
-
-Vue.use(Vuetify, { iconfont: 'mdi' })
